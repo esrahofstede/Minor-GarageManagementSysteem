fbaaf566a5a4579c1729189b601269e9d3d8ec00
Pim Verheij <pimverheij@gmail.com>
2015-11-18 15:58:23 +0100

    Trying to connect with RDW, Encoding has to be utf-8 instead of utf-16

---
+++ 31-ISRijksdienstWegVerkeer/Minor.Case2.ISRDW.Implementatie/RDWAdapter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Minor.Case2.ISRDW.Implementation
+{
+    public class RDWAdapter
+    {
+
+        public string CreateMessage()
+        {
+            apkKeuringsverzoekRequestMessage message = new apkKeuringsverzoekRequestMessage
+            {
+                keuringsverzoek = new keuringsverzoek
+                {
+                    keuringsdatum = new DateTime(2008, 11, 19),
+                    keuringsinstantie = new keuringsinstantie
+                    {
+                        kvk = "3013 5370",
+                        type = "garage",
+                        naam = "Garage Voorbeeld B.V.",
+                        plaats = "Wijk bij Voorbeeld",
+                    },
+                    voertuig = new keuringsverzoekVoertuig
+                    {
+                        kenteken = "BV-01-EG",
+                        kilometerstand = 12345,
+                        naam = "A. Eigenaar",
+                        type = "personenauto",
+                    },
+                    correlatieId = "0038c17b-aa10-4f93-8569-d184fdfc265b",
+                }
+            };
+
+            return UsingStream(message);
+        }
+
+
+        private string UsingStream(apkKeuringsverzoekRequestMessage myClass)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var xw = XmlWriter.Create(ms)) // Remember to stop using XmlTextWriter  
+                {
+                    var serializer = new XmlSerializer(myClass.GetType());
+                    serializer.Serialize(xw, myClass);
+                    xw.Flush();
+                    ms.Seek(0, SeekOrigin.Begin);
+                    using (var sr = new StreamReader(ms, Encoding.UTF8))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        public RDWAdapter()
+        {
+
+        }
+
+        private string GetMessage(string url)
+        {
+            WebRequest request = HttpWebRequest.Create(url);
+            request.Method = "GET";
+
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string returnMessage = reader.ReadToEnd();
+            return returnMessage;
+        }
+
+        public string SubmitAPKVerzoek(string url, string message)
+        {
+            return PostMessage(url, message);
+        }
+
+        private string PostMessage(string url, string message)
+        {
+            byte[] bodyBytes = Encoding.UTF8.GetBytes(message);
+            WebRequest request = HttpWebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "text/xml";
+            request.ContentLength = bodyBytes.Length;
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(bodyBytes, 0, bodyBytes.Length);
+
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string returnMessage = reader.ReadToEnd();
+            return returnMessage;
+        }
+    }
+
+    
+
+}
---
+++ 31-ISRijksdienstWegVerkeer/Minor.Case2.ISRDW.Implementation.Tests/RDWAdapterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Minor.Case2.ISRDW.Implementation.Tests
+{
+    [TestClass]
+    public class RDWAdapterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string urlRDW = @"http://rdwserver:18423/rdw/APKKeuringsverzoek";
+            RDWAdapter adapter = new RDWAdapter();
+
+            string message = adapter.CreateMessage();
+
+
+            //Act
+
+
+            var result = adapter.SubmitAPKVerzoek(urlRDW, message);
+
+
+            //Assert
+        }
+    }
+}
