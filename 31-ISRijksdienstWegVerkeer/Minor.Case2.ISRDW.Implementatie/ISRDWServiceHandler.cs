babd4231f7adaf6ab936619f19c4b375aa0d3b15
Pim Verheij <pimverheij@gmail.com>
2015-11-18 14:24:56 +0100

    Initial Intergration service project

---
+++ 31-ISRijksdienstWegVerkeer/Minor.Case2.ISRDW.Implementatie/ISRDWServiceHandler.cs
@@ -0,0 +1,20 @@
+﻿using Minor.Case2.ISRDW.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Minor.Case2.ISRDW.Implementation
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ISRDWServiceHandler : IISRDWService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+    }
+}
