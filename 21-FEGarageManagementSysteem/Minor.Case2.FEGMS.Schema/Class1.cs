dddc4c53b1752bef8a7dac70b2cc312ea1e750b0
Caspar Eldermans <caspar.eldermans@gmail.com>
2015-11-18 14:22:38 +0100

    Added Client

---
+++ 21-FEGarageManagementSysteem/Minor.Case2.FEGMS.Schema/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minor.Case2.FEGMS.Schema
+{
+    public class Class1
+    {
+    }
+}
