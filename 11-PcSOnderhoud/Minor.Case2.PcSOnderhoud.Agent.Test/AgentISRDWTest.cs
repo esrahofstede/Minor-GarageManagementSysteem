a865cbb73df7442e2b5a2aa8c5d75491309fa278
Esra Hofstede <esrahofstede@gmail.com>
2015-11-19 15:53:37 +0100

    Locator

---
+++ 11-PcSOnderhoud/Minor.Case2.PcSOnderhoud.Agent.Test/AgentISRDWTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Minor.Case2.PcSOnderhoud.Agent.Tests
+{
+    [TestClass]
+    public class AgentISRDWTest
+    {
+        [TestMethod]
+        public void CreateAgentTest()
+        {
+            //Arrange
+            AgentISRDW agent = new AgentISRDW();
+            //Act
+            //Assert
+        }
+    }
+}
