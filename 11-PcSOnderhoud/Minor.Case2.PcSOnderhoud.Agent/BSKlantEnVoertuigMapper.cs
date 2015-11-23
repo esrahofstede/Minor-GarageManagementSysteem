b4ab6a2a7fd60ce88ed176962426c02516c5efdc
Esra Hofstede <esrahofstede@gmail.com>
2015-11-23 14:49:21 +0100

    implemented mapper tussen pcs en bcs

---
+++ 11-PcSOnderhoud/Minor.Case2.PcSOnderhoud.Agent/BSKlantEnVoertuigMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minor.Case2.PcSOnderhoud.Agent
+{
+    public class BSKlantEnVoertuigMapper
+    {
+        
+    }
+}
